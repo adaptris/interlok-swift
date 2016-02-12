ce8d9291a72f13ae7fca74c6c72c68795e7291fa
Lewin Chan <lewin.chan@adaptris.com>
2016-02-12 11:13:13 +0000

    INTERLOK-732 added @ComponentProfile for SWIFT INTERLOK-732 #time 2m

--- src/com/adaptris/core/transform/swift/SwiftXmlService.java
+++ src/com/adaptris/core/transform/swift/SwiftXmlService.java
@@ -6,6 +6,8 @@
  */
 package com.adaptris.core.transform.swift;
 
+import com.adaptris.annotation.AdapterComponent;
+import com.adaptris.annotation.ComponentProfile;
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.CoreException;
 import com.adaptris.core.ServiceException;
@@ -28,6 +30,8 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
  * 
  */
 @XStreamAlias("swift-xml-service")
+@AdapterComponent
+@ComponentProfile(summary = "Transform a SWIFT message to XML", tag = "service,transform,xml,swift")
 public class SwiftXmlService extends LicensedService {
 
   /**
